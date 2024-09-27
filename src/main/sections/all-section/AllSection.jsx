a87bedada061e1b0673fbe8f1ab0600406835cf3
Pardeep <pradeep.singh@altudo.co>
2024-09-27 16:52:52 +0530

    Reaverted

--- src/main/sections/all-section/AllSection.jsx
+++ src/main/sections/all-section/AllSection.jsx
@@ -1,31 +1,11 @@
 import React from "react";
+import { ExploreSectionCard } from "../../../components";
 import "./Section.css";
-import src from "../../../assets/images/skills_avatar.png";
 
 const AllSection = ({ data, handleExploreAllClick }) => {
   return (
     <div className="section">
-      <div className="all_sections">
-        {data.cards.map((card, index) => (
-          <div
-            className={`item item_${index + 1}`}
-            key={index}
-            onClick={() => handleExploreAllClick(card.title.toLowerCase())}
-          >
-            <p className="text">{card?.title}</p>
-          </div>
-        ))}
-        <div className="item item_5">
-          <img
-            className="all_section_avatar"
-            src={src}
-            height={200}
-            width={200}
-            alt="avatar-icon"
-          />
-        </div>
-      </div>
-      {/* <div className="cards_section">
+      <div className="cards_section">
         {data.cards.map((card, index) => (
           <ExploreSectionCard
             key={index}
@@ -34,7 +14,7 @@ const AllSection = ({ data, handleExploreAllClick }) => {
             handleExploreAllClick={handleExploreAllClick}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
