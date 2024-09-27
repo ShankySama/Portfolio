6ae36d544d0aed6f913237ac99ccbd49f384884d
Pardeep <pradeep.singh@altudo.co>
2024-09-27 11:06:25 +0530

    Working on animation

--- src/main/sections/all-section/AllSection.jsx
+++ src/main/sections/all-section/AllSection.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { ExploreSectionCard } from "../../../components";
 import "./Section.css";
+import src from "../../../assets/images/skills_avatar.png";
 
 const AllSection = ({ data, handleExploreAllClick }) => {
   return (
     <div className="section">
+      {/* <div className="preLoader">
+        <div className="item item_1">
+          <p className="text">Skills</p>
+        </div>
+        <div className="item item_2">
+          <p className="text">Projects</p>
+        </div>
+        <div className="item item_3">
+          <p className="text">Experience</p>
+        </div>
+        <div className="item item_4">
+          <p className="text">Interests</p>
+        </div>
+        <div className="item item_5">
+          <img src={src} height={300} width={300}/>
+        </div>
+      </div> */}
       <div className="cards_section">
         {data.cards.map((card, index) => (
           <ExploreSectionCard
